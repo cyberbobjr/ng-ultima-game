e740dafb0e32ffa523cb38fd0bd327dda205ef78
Benjamin MARCHAND <cyberbobjr@gmail.com>
2017-04-14 17:42:06 +0200

    add speed system

--- src/app/systems/movement.system.ts
+++ src/app/systems/movement.system.ts
@@ -9,10 +9,13 @@ import {DescriptionsService} from "../services/informations/descriptions.service
 import {IMap} from "../interfaces/IMap";
 import {ScenegraphService} from "../services/scene-graph/scenegraph.service";
 import * as _ from "lodash";
+import {ITile} from "../interfaces/ITile";
+import {TilesLoaderService} from "../services/tiles/tiles.service";
 
 @Injectable()
 export class MovementSystem {
     constructor(private _entities: EntitiesService,
+                private _tilesService: TilesLoaderService,
                 private _mapService: MapsService,
                 private _informationsService: DescriptionsService,
                 private _scenesService: ScenegraphService) {
@@ -33,13 +36,10 @@ export class MovementSystem {
         let positionBehavior = <PositionBehavior>entity.getBehavior("position");
         let destinationPosition = this._getEntityDestinationPosition(positionBehavior, movableBehavior);
         if (this._mapService.isTileAtPositionIsWalkable(destinationPosition)) {
-            this._processWalkableMovement(positionBehavior, movableBehavior, destinationPosition);
             if (this._isLeaveCity(destinationPosition)) {
-                let newPosition: Position = this._mapService.getPositionOfCity(destinationPosition.mapId);
-                this._scenesService.setMapForEntity(entity, newPosition)
-                    .then(() => {
-                        this._informationsService.addTextToInformation("Leave the city");
-                    });
+                this._processLeaveCity(entity, destinationPosition);
+            } else {
+                this._processWalkableMovement(entity, destinationPosition);
             }
         } else {
             this._informationsService.addTextToInformation("Blocked!");
@@ -47,13 +47,33 @@ export class MovementSystem {
         movableBehavior.stay();
     }
 
+    private _processLeaveCity(entity: Entity, destinationPosition: Position) {
+        let newPosition: Position = this._mapService.getPositionOfCity(destinationPosition.mapId);
+        this._scenesService.setMapForEntity(entity, newPosition)
+            .then(() => {
+                this._informationsService.addTextToInformation("LEAVING...");
+            });
+    }
+
     private _getEntityDestinationPosition(currentEntityPosition: PositionBehavior, entityDirection: MovableBehavior): Position {
         return currentEntityPosition.position.addVector(entityDirection.vector);
     }
 
-    private _processWalkableMovement(positionBehavior: PositionBehavior, movableBehavior: MovableBehavior, destinationPosition: Position) {
-        positionBehavior.moveTo(movableBehavior.vector);
-        this._displayMoveInformation(movableBehavior.vector);
+    private _processWalkableMovement(entity: Entity, destinationPosition: Position) {
+        let tile: ITile = this._mapService.getTileAtPosition(destinationPosition);
+        let speed = this._tilesService.getTileSpeed(tile.name);
+        if (this._canMove(speed)) {
+            let movableBehavior = <MovableBehavior>entity.getBehavior("movable");
+            let positionBehavior = <PositionBehavior>entity.getBehavior("position");
+            positionBehavior.moveTo(movableBehavior.vector);
+            this._displayMoveInformation(movableBehavior.vector);
+        } else {
+            this._informationsService.addTextToInformation("Slow progress!");
+        }
+    }
+
+    private _canMove(speedTile: number): boolean {
+        return (Math.floor(Math.random() * speedTile) + 1 === 1);
     }
 
     private _displayMoveInformation(vector: Position) {
