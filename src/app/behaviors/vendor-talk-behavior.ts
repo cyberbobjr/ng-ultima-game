133dbdd9d30d3ea2dc126d27be21884846eae58b
Benjamin MARCHAND <cyberbobjr@gmail.com>
2017-05-05 00:12:43 +0200

    WIP - vendor conversation system

--- src/app/behaviors/vendor-talk-behavior.ts
+++ src/app/behaviors/vendor-talk-behavior.ts
@@ -185,11 +185,19 @@ export class VendorTalkBehavior extends TalkBehavior {
             return "What ?";
         }
         let totalAmount: number = this._itemTransaction.price * input;
-        this._removeGoldToParty(totalAmount);
+        if (totalAmount > this._getEntityGold(this._talkTo)) {
+            this._talkStatut = talkStatus.wait_for_anything_else;
+            return [
+                "I fear you have not the funds, perhaps something else.",
+                "Anything else?"
+            ];
+        } else {
+            this._removeGoldToParty(totalAmount);
+        }
     }
 
     private _removeGoldToParty(totalAmount: number) {
-        let partyBehavior: PartyBehavior = <PartyBehavior>this._talkTo.getBehavior("inventtory");
+        let partyBehavior: PartyBehavior = <PartyBehavior>this._talkTo.getBehavior("party");
         partyBehavior.removeGold(totalAmount);
     }
 
